c55a8551bb7be2389a6e66e80422d1f8aae1891c
Tomohiro Arakawa <tomohiro@tarakawa.net>
2021-03-25 10:47:25 -0400

    Comment out watches for now

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
-const { connect } = require('@oada/client');
-const oadalist  = require('@oada/list-lib');
-const trees = require('@pork/trees');
-const express = require('express');
-const config = require('./config');
-const debug = require('debug');
+const { connect } = require("@oada/client");
+const oadalist = require("@oada/list-lib");
+const trees = require("@pork/trees");
+const express = require("express");
+const config = require("./config");
+const debug = require("debug");
 
-const trace = debug('snatchbot:trace');
-const info = debug('snatchbot:info');
-const warn = debug('snatchbot:warn');
-const error = debug('snatchbot:error');
+const trace = debug("snatchbot:trace");
+const info = debug("snatchbot:info");
+const warn = debug("snatchbot:warn");
+const error = debug("snatchbot:error");
 
 const ListWatch = oadalist.ListWatch; // not sure why I can't just import this directly
 
-const domain = config.get('domain');
-const token = config.get('token');
+const domain = config.get("domain");
+const token = config.get("token");
 
 (async () => {
+  // if (domain === 'localhost') {
+  //   process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
+  //   warn('WARNING: doman is localhost, automatically accepting self-signed ssl certs');
+  // }
 
-if (domain === 'localhost') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
-  warn('WARNING: doman is localhost, automatically accepting self-signed ssl certs');
-}
+  // const oada = await connect({domain,token});
 
-const oada = await connect({domain,token});
+  // // ensure the thing exists because we are in charge of this endpoint
+  // trace('Checking if tree exists...');
+  // const exists = await oada.get({ path: `/bookmarks/trellisfw/asns`}).then(r=>r.status).catch(e => e.status);
+  // if (exists !== 200) {
+  //    info(`/bookmarks/trellisfw/asns does not exist, creating....`);
+  //    await oada.put({path: '/bookmarks/trellisfw/asns', data: {}, tree: trees.asn});
+  // }
 
-// ensure the thing exists because we are in charge of this endpoint
-trace('Checking if tree exists...');
-const exists = await oada.get({ path: `/bookmarks/trellisfw/asns`}).then(r=>r.status).catch(e => e.status);
-if (exists !== 200) {
-   info(`/bookmarks/trellisfw/asns does not exist, creating....`);
-   await oada.put({path: '/bookmarks/trellisfw/asns', data: {}, tree: trees.asn});
-}
+  // const asnWritten = async (item, key) => {
+  //   console.log('onItem called!  key = ', key, ', item = ', item);
+  // }
 
+  // trace('Setting up watch...');
+  // const watch = new ListWatch({
+  //   path: `/bookmarks/trellisfw/asns`,
+  //   // Need tree and itemsPath for this to work
+  //   tree: trees.asn,
+  //   itemsPath: `$.day-index.*.*`,
+  //   name: 'SNATCHBOT-PORKHACK',
+  //   conn: oada,
+  //   resume: true,
 
-const asnWritten = async (item, key) => {
-  console.log('onItem called!  key = ', key, ', item = ', item);
-}
+  //   //onAddItem: asnAdded,
+  //   //onChangeItem: asnAdded,
+  //   onItem: asnWritten,
 
-trace('Setting up watch...');
-const watch = new ListWatch({
-  path: `/bookmarks/trellisfw/asns`,
-  // Need tree and itemsPath for this to work
-  tree: trees.asn,
-  itemsPath: `$.day-index.*.*`,
-  name: 'SNATCHBOT-PORKHACK',
-  conn: oada,
-  resume: true,
+  //   onNewList: ListWatch.AssumeHandled,
+  //   // TODO: onDeleteList
+  // });
 
-  //onAddItem: asnAdded,
-  //onChangeItem: asnAdded,
-  onItem: asnWritten,
+  trace("Setting up express");
+  const app = express();
 
-  onNewList: ListWatch.AssumeHandled,
-  // TODO: onDeleteList
-});
+  app.get("/", async (req, res) => {
+    console.log("The snatchbot endpoint was called!  req = ", req);
 
-
-trace('Setting up express');
-const app = express();
-
-app.get('/', async (req, res) => {
-  console.log('The snatchbot endpoint was called!  req = ', req);
-
-  /*
+    /*
   if (!req || !req.headers) {
     trace('no headers!');
     return res.end();
@@ -70,13 +67,13 @@ app.get('/', async (req, res) => {
   if (req.headers.authorization !== 'Bearer '+incomingToken) {
     info('Request for check: Not the right token');
     return res.end();
-  }*/
+    }*/
 
+    res.json({ hello: "test" });
+  });
 
-});
-
-
-trace('Starting express....');
-app.listen(config.get('port'), () => console.log(`snatchbot listening on port ${config.get('port')}`));
-
+  trace("Starting express....");
+  app.listen(config.get("port"), () =>
+    console.log(`snatchbot listening on port ${config.get("port")}`)
+  );
 })();
